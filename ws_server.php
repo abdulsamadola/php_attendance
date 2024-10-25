7380249c145d8a49e0524dab9c235660f28818a9
abdulsamadola <suleimanolamilekan03@gmail.com>
2024-10-25 16:43:10 +0100

    Remove json ecncode

--- ws_server.php
+++ ws_server.php
@@ -35,7 +35,7 @@ function sendDataToApi($url, $data) {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
     curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
     $response = curl_exec($ch);
     curl_close($ch);
     
